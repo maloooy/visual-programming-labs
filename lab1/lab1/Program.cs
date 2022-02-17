26f12a421102ae85c45694871f4175b51c41d5e1
Mikhail Basargin <78631231+maloooy@users.noreply.github.com>
2022-02-17 20:32:44 +0700

    add 1 lab

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[3] { 5, 3, 4 };
+            Console.WriteLine(HW1.QueueTime(a, 1));
+            //int[] a = new int[4] { 10, 2, 3, 3 };
+            //Console.WriteLine(HW1.QueueTime(a, 2));
+            //int[] a = new int[3] { 2, 3, 10 };
+            //Console.WriteLine(HW1.QueueTime(a, 2));
+        }
+    }
+
+    public class HW1
+    {
+        public static long QueueTime(int[] customers, int n)
+        {
+            var place = new int[n];
+
+            for (int i = 0; i < customers.Length; i++)
+            {
+                place[0] += customers[i];
+                Array.Sort(place);
+            }
+
+            return place[n - 1];
+        }
+    }
+
+
+}
